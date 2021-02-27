def107a2af0960cf1ae32399295358bb8922c134
user <stancescu2000@gmail.com>
2021-02-27 20:32:31 +0200

    clasa Persoana

--- sem1/src/sem1/Test.java
+++ sem1/src/sem1/Test.java
@@ -5,5 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Hello world! 2");
+		
+		Persoana p1=new Persoana("Chuck", "Norris");
+		
+		System.out.println(p1);
 	}
 }
